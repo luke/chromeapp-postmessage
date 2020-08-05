d8b3a53ee5dc5aba126728108c9e733935558cfb
natp0ng <nattapong.kongmun@gmail.com>
2020-08-05 21:27:50 +0700

    update

--- www/first.js
+++ www/first.js
@@ -3,7 +3,6 @@ window.onload = () => {
     document.getElementsByTagName("iframe")[0].contentWindow.postMessage('hello from first window', '*')
   }
   document.getElementById('first_btn2').onclick = (e) => {
-    console.info('>>>>>>', e.source)
     e.source.postMessage('hello from first window', e.source.origin)
   }
   window.addEventListener("message", function(event) {
